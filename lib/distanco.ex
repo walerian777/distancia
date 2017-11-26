95b797fc3e57103cb1a0987408f2569087a45847
Walerian <walerian.sobczak@getbase.com>
2017-11-26 11:08:18 +0100

    Mix new

---
+++ lib/distanco.ex
@@ -0,0 +1,18 @@
+defmodule Distanco do
+  @moduledoc """
+  Documentation for Distanco.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Distanco.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/distanco_test.exs
@@ -0,0 +1,4 @@
+defmodule DistancoTest do
+  use ExUnit.Case
+  doctest Distanco
+end
