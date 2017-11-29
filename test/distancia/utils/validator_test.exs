a0617d9d507b80f1e59d89eb44aa3f23ce1af169
Walerian <walerian.sobczak@getbase.com>
2017-11-30 00:55:33 +0100

    Added tests for the Validator module.

---
+++ test/distancia/utils/validator_test.exs
@@ -0,0 +1,61 @@
+defmodule Distancia.Utils.ValidatorTest do
+  use ExUnit.Case
+
+  alias Distancia.Utils.Validator
+
+  test "validate returns :ok for two lists" do
+    a = [1, 2]
+    b = [3, 4]
+    assert { :ok, _ } = Validator.validate(a, b)
+  end
+
+  test "validate returns :ok for two tuple" do
+    a = {1, 2}
+    b = {3, 4}
+    assert { :ok, _ } = Validator.validate(a, b)
+  end
+
+  test "validate returns :ok for two numbers" do
+    a = 1
+    b = 3
+    assert { :ok, _ } = Validator.validate(a, b)
+  end
+
+  test "validate returns :ok for two binaries" do
+    a = "12"
+    b = "34"
+    assert { :ok, _ } = Validator.validate(a, b)
+  end
+
+  test "validate returns array of arguments when they are valid" do
+    a = [1, 2]
+    b = [3, 4]
+    assert { :ok, [^a, ^b] } = Validator.validate(a, b)
+  end
+
+  test "validate returs :error for arguments of different length" do
+    a = [1, 2]
+    b = [3, 4, 5]
+    assert { :error, _ } = Validator.validate(a, b)
+  end
+
+  test "validate returs :error for arguments of different type" do
+    a = [1, 2]
+    b = {3, 4}
+    assert { :error, _ } = Validator.validate(a, b)
+  end
+
+  test "validate! returns the result for :ok" do
+    a = [1, 2]
+    b = [3, 4]
+    assert [^a, ^b] = Validator.validate!(a, b)
+  end
+
+  test "validate! raises an ArgumentError for :error" do
+    a = [1, 2]
+    b = [3, 4, 5]
+    assert_raise ArgumentError, fn ->
+      Validator.validate!(a, b)
+    end
+  end
+end
