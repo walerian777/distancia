3f92ff4bcd612fa22a9a7c750d11d2a92a61e08a
Walerian <walerian.sobczak@getbase.com>
2017-11-30 19:15:07 +0100

    Added a Parser module for parsing the input.

---
+++ lib/distancia/utils/parser.ex
@@ -0,0 +1,12 @@
+defmodule Distancia.Utils.Parser do
+  def map_to_lists(args) do
+    Enum.map(args, &to_list/1)
+  end
+
+  def to_list(arg), do: parse_to_list(arg)
+
+  defp parse_to_list(list) when is_list(list), do: list
+  defp parse_to_list(tuple) when is_tuple(tuple), do: Tuple.to_list(tuple)
+  defp parse_to_list(number) when is_number(number), do: [number]
+  defp parse_to_list(binary) when is_binary(binary), do: String.to_charlist(binary)
+end
