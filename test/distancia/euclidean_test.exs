5b09d6cf7be39b688a58aa35730489473b13d847
Walerian <walerian.sobczak@getbase.com>
2017-12-01 01:05:23 +0100

    Added tests for the Euclidean module.

---
+++ test/distancia/euclidean_test.exs
@@ -0,0 +1,35 @@
+defmodule Distancia.EuclideanTest do
+  use ExUnit.Case
+
+  alias Distancia.Euclidean
+
+  test "calculate returns valid distance (basic)" do
+    a = [1, 1]
+    b = [4, 5]
+
+    assert Euclidean.calculate(a, b) == 5
+  end
+
+  test "calculate returns valid distance (advanced)" do
+    a = [2, 4, 5, 3, 8, 2]
+    b = [3, 1, 5, -3, 7, 2]
+
+    assert Euclidean.calculate(a, b) == 6.855654600401044
+  end
+
+  test "calculate returns zero distance for two the same points" do
+    a = b = [0, 0]
+
+    assert Euclidean.calculate(a, b) == 0
+  end
+
+  test "calculate is deterministic" do
+    a = :rand.normal()
+    b = :rand.normal()
+
+    first_result = Euclidean.calculate(a, b)
+    second_result = Euclidean.calculate(a, b)
+
+    assert first_result == second_result
+  end
+end
