30a97b542f5463480e89e15e0db224d4479845d6
Walerian <walerian.sobczak@getbase.com>
2017-12-02 18:07:23 +0100

    Added a Chebyshev module for calculating the Chebyshev distance.

---
+++ lib/distancia/chebyshev.ex
@@ -0,0 +1,12 @@
+defmodule Distancia.Chebyshev do
+  alias Distancia.Utils.Parser
+  alias Distancia.Utils.Validator
+
+  def calculate(p, q) do
+    Validator.validate!(p, q)
+    |> Parser.map_to_lists()
+    |> Enum.zip()
+    |> Enum.map(fn({a, b}) -> abs(a - b) end)
+    |> Enum.max()
+  end
+end
