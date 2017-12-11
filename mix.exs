74cd29c951eaca947470bb1803122a6b669c1273
Walerian <walerian.sobczak@getbase.com>
2017-12-11 22:03:21 +0100

    Bumped to version 0.1.2.

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Distancia.Mixfile do
   def project do
     [
       app: :distancia,
-      version: "0.1.1",
+      version: "0.1.2",
       elixir: "~> 1.4",
       start_permanent: Mix.env == :prod,
       description: description(),
